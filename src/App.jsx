ef434587cf739ac0a7fef7c00794283f4ac3f981
Lengs12 <mahesaerlangga1227@gmail.com>
2025-05-22 16:28:29 +0700

    Home and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 // import komponen disini (import namanya)
 import Banner from './component/Banner'
+import Contact from './component/Contact'
 
 function App() {
   return (
     <>
       {/* trus panggil nama komponennya disini */}
       <Banner/>
+      <Contact/>
     </>
   )
 }
